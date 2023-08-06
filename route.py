1981160c55518c28a783a951ad732ff4f021e57e
Shaun Homer <shaunhomer1@hotmail.com>
2023-08-06 19:40:00 +0100

    updated html files

--- route.py
+++ route.py
@@ -6,4 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
