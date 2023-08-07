3f4fc3976174c26dc5c330a533bedf12cf4bac13
Shaun Homer <shaunhomer1@hotmail.com>
2023-08-07 18:00:00 +0100

    updates to navbar,footer,routes and other html and css files

--- route.py
+++ route.py
@@ -3,12 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-
+# Error Pages
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -19,6 +14,32 @@ def page_not_found(e):
     return render_template("500.html"), 500
 
 
+# All html webpages
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/account')
+def account():
+    return render_template("account.html")
+
+
+@app.route('/logout')
+def logout():
+    return render_template("logout.html")
