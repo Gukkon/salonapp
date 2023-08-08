1ae321c4026484cab0780b64960aa246204ce3c0
Shaun Homer <shaunhomer1@hotmail.com>
2023-08-08 21:19:42 +0100

    new images and home page set up

--- route.py
+++ route.py
@@ -20,11 +20,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-
 @app.route('/login')
 def login():
     return render_template("login.html")
