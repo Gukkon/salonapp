e22bd9b7bae49b7277f0853284da76a30974ac82
Shaun Homer <shaunhomer1@hotmail.com>
2023-08-11 18:04:16 +0100

    css changes to home page and log in section started

--- route.py
+++ route.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# Secret Key
+
+# Initialize The Database
+db = SQLAlchemy(app)
+app.app_context().push()
+
+# Create Model (EDIT THIS !!! MAY NEED TO CREATE A MODELS.PY FILE)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    #Create A String
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 # Error Pages
 @app.errorhandler(404)
@@ -15,12 +37,12 @@ def page_not_found(e):
 
 
 # All html webpages
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
     return render_template("index.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     return render_template("login.html")
 
