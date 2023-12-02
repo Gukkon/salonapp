d7ad1c3031a64bf348f51ff040488f0b3a14522b
Shaun Homer <shaunhomer1@hotmail.com>
2023-12-02 17:31:59 +0000

    .env

--- Salon_app/__init__.py
+++ Salon_app/__init__.py
@@ -35,3 +35,40 @@ login_manager = LoginManager(app)
 
 
 from Salon_app import routes
+
+# from Salon_app import routes
+# import os
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_login import LoginManager
+# from decouple import config
+
+# app = Flask(__name__)
+
+# # Secret Key
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+# # Add Database
+# DATABASE_URL = config('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+# # Ensure that you set this to False on production, it's True for development only
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+# app.config['SESSION_COOKIE_SECURE'] = True
+
+# # Initialize The Database Instance
+# db = SQLAlchemy(app)
+# bcrypt = Bcrypt(app)
+# login_manager = LoginManager(app)
+
+# # Additional configuration if needed for Flask-Login
+# login_manager.login_view = 'login'
+# login_manager.login_message_category = 'info'
+
+
+# if __name__ == "__main__":
+#     # Ensure that the app is run only when executed directly, not when imported as a module
+#     app.run(debug=True)
--- run.py
+++ run.py
@@ -1,5 +1,22 @@
-from Salon_app import app, db
+import os
+from Salon_app import app
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
+
+# import os
+# from Salon_app import app, db
+
+# # Check if the app is running on Heroku
+# is_heroku = os.environ.get('IS_HEROKU', None)
+
+# if is_heroku:
+#     # If running on Heroku, create tables and run the app
+#     db.create_all()
+# else:
+#     # If running locally, run the app with debug mode
+#     app.run(debug=True)
