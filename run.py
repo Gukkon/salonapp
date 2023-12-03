6098f34ee35e08550ce6d8ec1f3799c0eefd21ea
Shaun Homer <shaunhomer1@hotmail.com>
2023-12-03 13:32:38 +0000

    uploaded responsive pic to readme

--- run.py
+++ run.py
@@ -7,16 +7,3 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
-
-# import os
-# from Salon_app import app, db
-
-# # Check if the app is running on Heroku
-# is_heroku = os.environ.get('IS_HEROKU', None)
-
-# if is_heroku:
-#     # If running on Heroku, create tables and run the app
-#     db.create_all()
-# else:
-#     # If running locally, run the app with debug mode
-#     app.run(debug=True)
